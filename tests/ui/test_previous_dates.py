461cfa4b1337523b0a457a5ee5b48747eef2afd9
Michael Thies <mail@mhthies.de>
2026-01-02 22:47:36 +0100

    Add system test test_previous_date_not_merged

--- tests/ui/test_previous_dates.py
+++ tests/ui/test_previous_dates.py
@@ -34,3 +34,46 @@ def test_previous_date_view_merged(page: Page, reset_database: None) -> None:
         re.compile(r"\s*14:30\s*–\s*16:00.*Zuvor geplante Zeiten:\s*13:30\s*–\s*15:00\s*", re.DOTALL)
     )
     helpers.assert_is_line_through(time_cell.get_by_text("13:30"))
+
+
+def test_previous_date_not_merged(page: Page, reset_database: None) -> None:
+    actions.login(page, 1, "orga")
+    actions.add_room(page, data.ROOM_SPORTPLAETZE)
+    actions.add_room(page, data.ROOM_PELIKANHALLE)
+    actions.add_category(page, data.CATEGORY_SPORT)
+    actions.add_entry(page, data.ENTRY_BEACH_VOLLEYBALL)
+    actions.add_entry(page, data.ENTRY_LOREM_IPSUM)
+
+    row = helpers.get_table_row_by_column_value(page, "Was?", "Beach-Volleyball")
+    row.get_by_role("link", name="Eintrag bearbeiten").click()
+    page.get_by_role("textbox", name="Beginn").fill("14:30")
+    page.get_by_role("combobox", name="Orte").press("Backspace")  # Delete "Sportplätze"
+    page.get_by_role("combobox", name="Orte").fill("Pelikanhalle")
+    page.get_by_role("textbox", name="Kommentar zum Ort").clear()
+    page.get_by_role("option", name="Pelikanhalle").click()
+    page.get_by_role("checkbox", name="Hinweis zur Verschiebung am vorherigen Termin im KüA-Plan anlegen").check()
+    page.get_by_role("button", name="Speichern").click()
+    success_alert = page.get_by_role("alert").filter(has_text="Erfolg")
+    expect(success_alert).to_be_visible()
+    success_alert.get_by_role("button", name="Close").click()
+
+    row = helpers.get_table_row_by_column_value(page, "Was?", "Beach-Volleyball")
+    expect(row).to_have_count(2)
+    entry_anchor = row.last.locator("xpath=(./td)[1]").get_attribute("id")
+    move_link = row.first.get_by_role("link", name="14:30")
+    expect(move_link).to_be_visible()
+    expect(move_link).to_have_attribute("href", re.compile(rf".*#{re.escape(entry_anchor)}"))
+
+    room = row.first.get_by_text("Sportplätze")
+    expect(room).to_be_visible()
+    helpers.assert_is_line_through(room)
+    expect(helpers.get_table_cell_by_header(row.first, "Wo?")).not_to_contain_text("Pelikanhalle")
+    time = row.first.get_by_text("13:30")
+    expect(time).to_be_visible()
+    helpers.assert_is_line_through(time)
+    expect(helpers.get_table_cell_by_header(row.first, "Wann?")).not_to_contain_text("14:30")
+
+    expect(row.last).to_contain_text("Pelikanhalle")
+    expect(row.last).not_to_contain_text("Sportplätze")
+    expect(row.last).to_contain_text("14:30")
+    expect(row.last).not_to_contain_text("13:30")

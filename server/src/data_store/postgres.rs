mhthies/online-kueaplan

commit 0a1d9ca21af51147a77d15b3c999f78bc4f17c11

Fix HTTP 500 API error on change_entry endpoint when only rooms (or nothing) is updated

server/src/data_store/postgres.rs
@@ -457,11 +457,18 @@ impl KueaPlanStoreFacade for PgDataStoreFacade {
             if let Some(category_id) = entry_data.category.as_ref() {
                 check_category_validity(category_id, current_event_id, connection)?;
             }
-            diesel::update(entries)
+            let result = diesel::update(entries)
                 .filter(id.eq(entry_id))
                 .set(entry_data)
-                .execute(connection)?;
-            Ok(())
+                .execute(connection);
+
+            match result {
+                Ok(_) => Ok(()),
+                // Ignore error "There are no changes to save. This query cannot be built" when
+                // nothing is changed (or only rooms are changed)
+                Err(diesel::result::Error::QueryBuilderError(_)) => Ok(()),
+                Err(e) => Err(e.into()),
+            }
         })
     }
 

commit 9c7e4fe5a32bd5973a1daa1fe05a6a2ed8af19f0

Fix data_store: Make sure to update last_changed timestamp of entry/announcement when only rooms have been updated
This allows a revert of the more complex error handling, introduced in 0a1d9ca21af51147a77d15b3c999f78bc4f17c11.

server/src/data_store/postgres.rs
@@ -459,18 +459,12 @@ impl KueaPlanStoreFacade for PgDataStoreFacade {
             if let Some(category_id) = entry_data.category.as_ref() {
                 check_categories_validity(&[*category_id], current_event_id, connection)?;
             }
-            let result = diesel::update(entries)
+            diesel::update(entries)
                 .filter(id.eq(entry_id))
-                .set(entry_data)
-                .execute(connection);
+                .set((entry_data, last_updated.eq(diesel::dsl::now)))
+                .execute(connection)?;
 
-            match result {
-                Ok(_) => Ok(()),
-                // Ignore error "There are no changes to save. This query cannot be built" when
-                // nothing is changed (or only rooms are changed)
-                Err(diesel::result::Error::QueryBuilderError(_)) => Ok(()),
-                Err(e) => Err(e.into()),
-            }
+            Ok(())
         })
     }
 
@@ -1006,7 +1000,7 @@ impl KueaPlanStoreFacade for PgDataStoreFacade {
             }
             diesel::update(announcements)
                 .filter(id.eq(announcement_id))
-                .set(announcement_data)
+                .set((announcement_data, last_updated.eq(diesel::dsl::now)))
                 .execute(connection)?;
             Ok(())
         })
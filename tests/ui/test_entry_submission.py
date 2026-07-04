26421eea968ce1316ed3953c5e709f2b775d00ab
Michael Thies <mail@mhthies.de>
2026-07-04 21:52:05 +0200

    Add tests for calendar date info and dynamic parallel entries list in submit entry form

--- tests/ui/test_entry_submission.py
+++ tests/ui/test_entry_submission.py
@@ -1,3 +1,5 @@
+import dataclasses
+import datetime
 import re
 import time
 
@@ -214,6 +216,104 @@ def test_submit_entry_form_tab_navigation(browser: Browser, page: Page, reset_da
 
     page.go_forward()
     expect(orga_comment_input).to_be_visible()
+
+
+def test_submit_entry_date_info_indicator(page: Page, browser: Browser, reset_database: None) -> None:
+    orga_context = browser.new_context()
+    orga_page = orga_context.new_page()
+    actions.login(orga_page, 1, "admin")
+    actions.enable_entry_submission(orga_page, True)
+
+    actions.login(page, 1, "user")
+    page.get_by_role("link", name="Eintrag einreichen").click()
+    expect(page).to_have_title(re.compile(r"Eintrag einreichen"))
+    page.get_by_role("tab", name="Zeit & Ort").click()
+    date_input = page.get_by_role("combobox", name="Tag")
+    begin_input = page.get_by_role("textbox", name="Beginn")
+
+    date_input.select_option("03.01. (Fr)")
+    begin_input.fill("02:00")
+    calendar_date_indicator = begin_input.locator("..").locator("#calendarDateInfo")
+    expect(calendar_date_indicator).to_be_visible()
+    expect(calendar_date_indicator).to_contain_text("Beginn am nächsten Kalendertag")
+    expect(calendar_date_indicator).to_contain_text("04.01. 02:00")
+    assert helpers.is_text_colored(calendar_date_indicator)
+
+    date_input.select_option("04.01. (Sa)")
+    expect(calendar_date_indicator).to_be_visible()
+    expect(calendar_date_indicator).to_contain_text("05.01.")
+
+    begin_input.fill("06:00")
+    expect(calendar_date_indicator).not_to_be_visible()
+
+
+def test_submit_entry_parallel_entries(page: Page, browser: Browser, reset_database: None) -> None:
+    orga_context = browser.new_context()
+    orga_page = orga_context.new_page()
+    actions.login(orga_page, 1, "admin")
+    actions.enable_entry_submission(orga_page, True)
+    actions.add_category(orga_page, data.CATEGORY_SPORT)
+    actions.add_room(orga_page, data.ROOM_SPORTPLAETZE)
+    actions.add_room(orga_page, data.ROOM_PELIKANHALLE)
+    actions.add_room(orga_page, data.ROOM_SEMINARRAUM)
+    actions.add_entry(
+        orga_page,
+        dataclasses.replace(
+            data.ENTRY_BEACH_VOLLEYBALL,
+            day=datetime.date(2025, 1, 1),
+            begin=datetime.time(18, 30),
+            duration=datetime.timedelta(hours=1, minutes=20, seconds=20),
+            time_comment="Schnell noch vor dem Plenum :)",
+        ),
+    )
+    actions.add_entry(orga_page, data.ENTRY_BEGRUESSUNGSPLENUM)  # from 20:00
+    actions.add_entry(orga_page, data.ENTRY_PLENUMSVORBEREITUNG)  # 19:30 – 20:00
+
+    actions.login(page, 1, "user")
+    page.get_by_role("link", name="Eintrag einreichen").click()
+    expect(page).to_have_title(re.compile(r"Eintrag einreichen"))
+    page.get_by_role("tab", name="Zeit & Ort").click()
+    page.get_by_role("combobox", name="Tag").select_option("01.01. (Mi)")
+    page.get_by_role("combobox", name="Orte").fill("Seminarraum Pelikanhalle")
+    page.get_by_role("option", name="Seminarraum Pelikanhalle").click()
+    begin_input = page.get_by_role("textbox", name="Beginn")
+    duration_input = page.get_by_role("textbox", name="Dauer")
+    parallel_entries_box = page.get_by_role("complementary", name="Parallele Einträge")
+    parallel_entries_overlays = parallel_entries_box.get_by_role("status")
+
+    begin_input.fill("08:00")
+    duration_input.fill("1:00")
+    expect(parallel_entries_overlays).not_to_be_visible()
+    expect(parallel_entries_box).to_contain_text("Keine parallelen Einträge")
+
+    begin_input.fill("18:00")
+    expect(parallel_entries_overlays).not_to_be_visible()
+    expect(parallel_entries_box).not_to_contain_text("Keine parallelen Einträge")
+    list_item = parallel_entries_box.get_by_role("listitem")
+    expect(list_item).to_have_count(1)
+    expect(list_item).to_contain_text("Beach-Volleyball")
+    expect(list_item).to_contain_text("18:30 – 19:50")
+
+    begin_input.fill("18:30")
+    expect(parallel_entries_overlays).not_to_be_visible()
+    expect(list_item).to_have_count(1)
+
+    begin_input.fill("19:30")
+    expect(list_item).to_have_count(3)
+    # Items should be ordered from most problematic to least problematic (and then by time)
+    items = list_item.all()
+    expect(items[0]).to_contain_text("Begrüßungsplenum")  # exclusive
+    expect(items[0]).to_contain_text("exklusiv")
+    expect(items[1]).to_contain_text("Plenums-Vorbereitung")  # room conflict
+    expect(items[1]).to_contain_text("Pelikanhalle, Seminarraum Pelikanhalle")
+    assert helpers.is_text_bold(items[1].get_by_text("Seminarraum Pelikanhalle"))
+    assert helpers.is_text_colored(items[1].get_by_text("Seminarraum Pelikanhalle"))
+    expect(items[2]).to_contain_text("Beach-Volleyball")  # no conflict
+    assert not helpers.is_text_colored(items[2].get_by_text("Sportplätze"))
+
+    duration_input.fill("test")
+    expect(parallel_entries_overlays).to_be_visible()
+    expect(parallel_entries_overlays).to_contain_text("Ungültige Eingabedaten")
 
 
 def test_submit_entry_workflow_with_review_after_publishing(browser: Browser, reset_database: None) -> None:
